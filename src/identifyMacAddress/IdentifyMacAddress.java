99189a212d24f08be85cbcfda1ac24803ede6715
akiyama <akiyama@akiyamanoMacBook-ea.local>
2021-04-16 18:09:10 +0900

    制作中

--- src/identifyMacAddress/IdentifyMacAddress.java
+++ src/identifyMacAddress/IdentifyMacAddress.java
@@ -1,7 +1,18 @@
 package identifyMacAddress;
 
+import java.io.IOException;
+
 public class IdentifyMacAddress {
-	public static void main(String[] args) {
+	public void identify(String[][] originalData) throws IOException{
+		boolean ad_known = false;
+		String[][] data;
+		for(String[] row:originalData) {
+			for(String[] advA:data) {
+				if(advA[0] == row[0]) {
+
+				}
+			}
+		}
 
 	}
 
