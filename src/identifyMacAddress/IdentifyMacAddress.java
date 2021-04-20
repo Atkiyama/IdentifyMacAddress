88f7fd01a5e2d86dd581f401bdc21ea1e6b68693
akiyama <akiyama@akiyamanoMacBook-ea.local>
2021-04-20 15:08:09 +0900

    新規追加

--- src/identifyMacAddress/IdentifyMacAddress.java
+++ src/identifyMacAddress/IdentifyMacAddress.java
@@ -1,9 +1,20 @@
 package identifyMacAddress;
 
+import java.io.IOException;
+import java.util.Objects;
+
 public class IdentifyMacAddress {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		// TODO 自動生成されたメソッド・スタブ
+		Read read = new Read();
+		Identify identify;
+		if(Objects.nonNull(args[2]))
+			 identify = new Identify(Integer.parseInt(args[0]),Integer.parseInt(args[1]),Integer.parseInt(args[2]));
+		else
+			 identify = new Identify(Integer.parseInt(args[0]),Integer.parseInt(args[1]));
+		identify.identify(read.read());
+
 
 	}
 
