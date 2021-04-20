a62958f282f8aa1ec6340cc7d609f6248369ea84
akiyama <akiyama@akiyamanoMacBook-ea.local>
2021-04-20 15:37:06 +0900

    型名を修正

--- src/identifyMacAddress/TestDrive.java
+++ src/identifyMacAddress/TestDrive.java
@@ -8,7 +8,7 @@ import java.util.Objects;
  * @author akiyama
  *
  */
-public class IdentifyMacAddress {
+public class TestDrive {
 
 	public static void main(String[] args) throws IOException {
 		// TODO 自動生成されたメソッド・スタブ
