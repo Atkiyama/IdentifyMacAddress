ca607e7bbb13656292f3ca2943539cffde3f0d31
akiyama <akiyama@akiyamanoMacBook-ea.local>
2021-04-28 17:29:26 +0900

    compile.sh追加

---
+++ src/compile.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+javac dataAnalyze/analyze/DataAnalyze.java
+javac identifyMacAddressSingle/drive/IdentifyMacAddress.java
+javac convertMacAddress/ConvertMacAddress.java
+echo "全javaファイルのコンパイルが完了しました"
