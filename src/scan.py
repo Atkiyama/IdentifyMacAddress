e9d0aae2ca96efc0c32f251cfcd3a97cf9096ede
akiyama <akiyama@akiyamanoMacBook-ea.local>
2021-05-01 02:02:47 +0900

    scan.py追加

---
+++ src/scan.py
@@ -0,0 +1,19 @@
+from bluepy.btle import Scanner, DefaultDelegate
+import datetime
+class ScanDelegate(DefaultDelegate):
+       def __init__(self):
+               DefaultDelegate.__init__(self)
+       def handleDiscovery(self, dev, isNewDev, isNewData):
+               t_now = datetime.datetime.now().time()
+               print("%s %s (%s) [%s] %s dBm" % (t_now, dev.addr, dev.addrType, dev.iface, dev.rssi), end="")
+               for (adtype, desc, value) in dev.getScanData():
+                     print(" %s(%s) = %s" % (desc, adtype, value), end="")
+               print("")
+scanner = Scanner().withDelegate(ScanDelegate())
+while True:
+       try:
+               scanner.scan(5.0)
+       except BLEException:
+               MSG('BTLE Exception while scanning')
+
+
