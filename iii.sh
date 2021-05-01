4d8ba7fb63d31e4864dc0d356b1a298fb6514f09
akiyama <akiyama@akiyamanoMacBook-ea.local>
2021-05-01 22:57:33 +0900

    compile.sh変更

--- iii.sh
+++
@@ -1,7 +0,0 @@
-#bin/bash
-
-sudo apt-get install git build-essential libglib2.0-dev
-git clone https://github.com/IanHarvey/bluepy.git
-cd bluepy
-python setup.py build
-sudo python setup.py install
--- src/compile.sh
+++ src/compile.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 javac dataAnalyze/analyze/DataAnalyze.java -encoding UTF-8
-javac identifyMacAddressSingle/drive/IdentifyMacAddress.java -encoding UTF-8
+javac identifyMacAddressSingle/IdentifyMacAddress.java -encoding UTF-8
 javac convertMacAddress/ConvertMacAddress.java -encoding UTF-8
 echo "全javaファイルのコンパイルが完了しました"
