07a487991f33f814f2af9155be4da6e01b1829af
akiyama <akiyama@akiyamanoMacBook-ea.local>
2021-05-12 23:27:05 +0900

    3更新

--- src/scan3.sh
+++ src/scan3.sh
@@ -3,9 +3,9 @@
 #このスクリプトはUbuntuで動かすことを想定しています
 #sudoで実行してください
 
-date > data/capture/$fileName.txt
+date > data/capture/fileName.txt
 echo "パケットキャプチャを開始します"
 while true
 do
-  python3 scan2.py >>data/capture/$fileName.txt
+  python3 scan2.py >>data/capture/fileName.txt
 done
