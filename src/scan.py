08e5d50b9392a1dd10db9a9a9dfe33b1360bf55e
akiyama <akiyama@akiyamanoMacBook-ea.local>
2021-05-12 16:52:16 +0900

    scan.py変更

--- src/scan.py
+++ src/scan.py
@@ -6,12 +6,10 @@ class ScanDelegate(DefaultDelegate):
        def handleDiscovery(self, dev, isNewDev, isNewData):
                t_now = datetime.datetime.now().time()
                print("%s %s (%s) [%s] %s dBm" % (t_now, dev.addr, dev.addrType, dev.iface, dev.rssi), end = "")
-               for (adtype, desc, value) in dev.getScanData():
-                     print(" %s(%s) = %s" % (desc, adtype, value), end = "")
                print("")
 scanner = Scanner().withDelegate(ScanDelegate())
 while True:
        try:
                scanner.scan(5.0)
-       except BLEException:
+       except BLLEException:
                MSG('BTLE Exception while scanning')
