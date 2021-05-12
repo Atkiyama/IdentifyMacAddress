3bbfe467b0b193b3fd48840fd9b69d8c54d39724
akiyama <akiyama@akiyamanoMacBook-ea.local>
2021-05-12 22:05:52 +0900

    scan3,4修正

--- scah3.sh
+++ scah3.sh
@@ -2,7 +2,9 @@
 
 #このスクリプトはUbuntuで動かすことを想定しています
 #sudoで実行してください
+echo "ファイル名を入力"
+read fileName
 while true
 do
-  python3 scan2.py >data/capture/$fileName.txt
+  python3 scan2.py >>data/capture/$fileName.txt
 done
--- scan4.sh
+++ scan4.sh
@@ -2,7 +2,5 @@
 
 #このスクリプトはUbuntuで動かすことを想定しています
 #sudoで実行してください
-date
-echo "BLEパケットのキャプチャを開始します"
 timeout 1h ./scan3.sh
 echo "パケットキャプチャが終了しました"
