a2f31523704f8219cc1fe2f8854a46c23ceb54de
akiyama <akiyama@akiyamanoMacBook-ea.local>
2021-05-14 14:27:58 +0900

    scan.py更新

--- src/scan.py
+++ src/scan.py
@@ -5,7 +5,6 @@ class ScanDelegate(DefaultDelegate):
                DefaultDelegate.__init__(self)
        def handleDiscovery(self, dev, isNewDev, isNewData):
                t_now = datetime.datetime.now().time()
-               print("%s %s (%s) [%s] %s dBm" % (t_now, dev.addr, dev.addrType, dev.iface, dev.rssi), end = "")
-               print("")
+               print("%s %s %s dBm" % (t_now, dev.addr, dev.rssi))
 scanner = Scanner().withDelegate(ScanDelegate())
 scanner.scan(3600.0)
