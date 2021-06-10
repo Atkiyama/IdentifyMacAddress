5e3d1d169d0a710562ac29927a5e155829895fba
akiyama <akiyama@akiyamanoMacBook-ea.local>
2021-06-10 20:03:43 +0900

    Identify.sh変更

--- src/IdentifyMacAddress.sh
+++ src/IdentifyMacAddress.sh
@@ -6,9 +6,8 @@ do
   do
     for T in {1..10}
     do
-      java identifyMacAddress/IdentifyMacAddress data/capture/convert/$n,convertData.csv $R $T >data/result/multi/$n/$R,$T.txt
+      java identifyMacAddress/IdentifyMacAddress data/capture/convert/$n,convertData.csv $R $T >data/result/multi/$n/$R,$T.txt &
     done
   done
-  echo "$n is done"
-  sleep 3m
+  sleep 5m
 done
