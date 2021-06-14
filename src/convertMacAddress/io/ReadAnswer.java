4d38a2e2e4a65c2173eb7bff0e2c1157666d732d
akiyama <akiyama@akiyamanoMacBook-ea.local>
2021-06-15 01:42:26 +0900

    singleresult is 15

--- src/convertMacAddress/io/ReadAnswer.java
+++ src/convertMacAddress/io/ReadAnswer.java
@@ -19,7 +19,7 @@ public class ReadAnswer {
 	 * @throws IOException
 	 */
 	public ArrayList<BTMachine> read() throws IOException {
-		File file = new File("data/result/singleResult10.csv");
+		File file = new File("data/result/singleResult15.csv");
 		FileReader fileReader = new FileReader(file);
 		BufferedReader in = new BufferedReader(fileReader);
 		ArrayList<BTMachine> btMachines = new ArrayList<>();
