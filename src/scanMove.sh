09f77231a3221eccc03ed49e83d7f76aaa7cccda
akiyama <akiyama@akiyamanoMacBook-ea.local>
2021-09-11 15:26:14 +0900

    scanのパス変更

--- src/scanMove.sh
+++ src/scanMove.sh
@@ -6,6 +6,6 @@ echo "ファイル名を入力"
 read fileName
 date
 echo "キャプチャを開始します"
-timeout 1h python3 scan.py > data/capture/move/$fileName.txt
+timeout 1h python3 scan.py > data/capture/single/move/$fileName.txt
 echo "パケットキャプチャが終了しました"
 date
