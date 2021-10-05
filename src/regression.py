1c6a22da3b70b895952fd6cca3d1fbbba2e38b60
akiyama <akiyama@akiyamanoMacBook-ea.local>
2021-10-05 12:51:05 +0900

    変更

--- src/regression.py
+++ src/regression.py
@@ -5,7 +5,7 @@ import numpy as np
 #コマンドライン引数 1閾値P
 args = sys.argv
 capture = pd.read_csv('data/regression/regression.csv', sep=",")
-P = int(args[5])
+P = int(args[1])
 #モデル生成
 from sklearn.linear_model import LinearRegression
 clf = LinearRegression(fit_intercept = True, normalize = False, copy_X = True, n_jobs = -1)
