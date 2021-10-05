81f3abe04dbade5c76ced91998a3f90119254b95
akiyama <akiyama@akiyamanoMacBook-ea.local>
2021-10-05 12:46:54 +0900

    いろいろ修正

--- src/identifyMove.sh
+++ src/identifyMove.sh
@@ -2,7 +2,7 @@
 
 #コマンドライン引数
 numOfData=$1
-
+date
 for n in {1..100}
 do
   for R in {1..20}
@@ -11,16 +11,10 @@ do
     do
       for P in {1..20}
       do
-        commandString='ps|grep java|wc -l'
-        count=$(eval $commandString)
-        while [ "$count" -gt "10" ]
-        do
-          commandString='ps|grep java|wc -l'
-          count=$(eval $commandString)
-        done
-          java identifyMacAddress/identify/IdentifyMove data/capture/convert/move/$numOfData/$n,convertData.csv $R $T $P $n >data/result/multi/move/$numOfData/$n/$R,$T,$P.txt 
+          java identifyMacAddress/identify/IdentifyMove data/capture/convert/move/$numOfData/$n,convertData.csv $R $T $P $n >data/result/multi/move/$numOfData/$n/$R,$T,$P.txt
       done
     done
   done
   #コレがないと現状落ちるのでいれておくこと(プロなら回避可能??)
 done
+date
