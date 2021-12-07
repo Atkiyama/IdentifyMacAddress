ae5881e924b993c45a046df118812310f1485acf
akiyama <akiyama@akiyamanoMacBook-ea.local>
2021-12-08 00:14:58 +0900

    add

--- src/identifyForR_I.sh
+++ src/identifyForR_I.sh
@@ -1,22 +1,8 @@
 #bin/bash
-
-
+#バックグラウンド側で実行しないこと
 numOfData=$1
-method=$2
-for R in {1..20}
+java delay/DelayForR_I $numOfData
+for method in bagging svr linerRegression
 do
-  for T in 6
-  do
-    for I in {1..20}
-    do
-      for n in {1..100}
-      do
-        ./identify $R $T $I $numOfData $n $method >data/result/multi/move/$method/$n.txt
-      done
-      if [ "$R" -eq "1" -a "$I" -eq "1" ]; then
-        echo "R,T,I,score" > data/result/evaluation/move/$numOfTime/$method,$numOfData.csv
-      fi
-      java evaluation/evaluation/Evaluation $R $T $I $method >> data/result/evaluation/move/$numOfTime/$method,$numOfData.csv
-    done
-  done
+  ./identifyForR_I_sub.sh $numOfData $method &
 done
