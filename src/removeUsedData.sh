7d569b2e8707cbd1439cf3fe58b3c7268713b575
秋山周平 <akiyamashuuhei@akiyamashuuheinoMacBook-Pro.local>
2022-02-21 14:34:19 +0900

    add

--- src/removeUsedData.sh
+++ src/removeUsedData.sh
@@ -1,20 +1,27 @@
 #bin/bash
 
 rm /Users/akiyamashuuhei/data/IdentifyMacAddress/src/data/address/processed/addressList/*
-rm /Users/akiyamashuuhei/data/IdentifyMacAddress/src/data/address/processed/fAddress/*
-rm /Users/akiyamashuuhei/data/IdentifyMacAddress/src/data/address/processed/lAddress/*
-rm -rf /Users/akiyamashuuhei/data/IdentifyMacAddress/src/data/address/processed/regression/*
+rm -rf /Users/akiyamashuuhei/data/IdentifyMacAddress/src/data/address/processed/fAddress
+rm -rf /Users/akiyamashuuhei/data/IdentifyMacAddress/src/data/address/processed/lAddress
+mkdir /Users/akiyamashuuhei/data/IdentifyMacAddress/src/data/address/processed/fAddress
+mkdir /Users/akiyamashuuhei/data/IdentifyMacAddress/src/data/address/processed/lAddress
+rm -rf /Users/akiyamashuuhei/data/IdentifyMacAddress/src/data/address/processed/regression
 mkdir  /Users/akiyamashuuhei/data/IdentifyMacAddress/src/data/address/processed/regression
 mkdir  /Users/akiyamashuuhei/data/IdentifyMacAddress/src/data/address/processed/regression/svr
 mkdir  /Users/akiyamashuuhei/data/IdentifyMacAddress/src/data/address/processed/regression/bagging
 mkdir  /Users/akiyamashuuhei/data/IdentifyMacAddress/src/data/address/processed/regression/linerRegression
 mkdir  /Users/akiyamashuuhei/data/IdentifyMacAddress/src/data/address/processed/regression/randomForest
-rm  /Users/akiyamashuuhei/data/IdentifyMacAddress/src/data/capture/convert/move/*
+rm -rf /Users/akiyamashuuhei/data/IdentifyMacAddress/src/data/capture/convert/move/
+mkdir /Users/akiyamashuuhei/data/IdentifyMacAddress/src/data/capture/convert/move
 rm -rf /Users/akiyamashuuhei/data/IdentifyMacAddress/src/data/result/multi/move
 mkdir  /Users/akiyamashuuhei/data/IdentifyMacAddress/src/data/result/multi/move
-mkdir  /Users/akiyamashuuhei/data/IdentifyMacAddress/src/data/result/multi/move/svr
+mkdir  /Users/akiyamashuuhei/data/IdentifyMacAddress/src/data/result/multi/move/svr/
+mkdir  /Users/akiyamashuuhei/data/IdentifyMacAddress/src/data/result/multi/move/svr/{0...100}
 mkdir  /Users/akiyamashuuhei/data/IdentifyMacAddress/src/data/result/multi/move/bagging
 mkdir  /Users/akiyamashuuhei/data/IdentifyMacAddress/src/data/result/multi/move/linerRegression
 mkdir  /Users/akiyamashuuhei/data/IdentifyMacAddress/src/data/result/multi/move/randomForest
 mkdir  /Users/akiyamashuuhei/data/IdentifyMacAddress/src/data/result/multi/move/old
- 
+mkdir  /Users/akiyamashuuhei/data/IdentifyMacAddress/src/data/result/multi/move/bagging/{0..100}
+mkdir  /Users/akiyamashuuhei/data/IdentifyMacAddress/src/data/result/multi/move/linerRegression/{0..100}
+mkdir  /Users/akiyamashuuhei/data/IdentifyMacAddress/src/data/result/multi/move/randomForest/{0..100}
+mkdir  /Users/akiyamashuuhei/data/IdentifyMacAddress/src/data/result/multi/move/old/{0..100}
