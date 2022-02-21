78cc777ec5868ae42807e3df733868f8212067cc
秋山周平 <akiyamashuuhei@akiyamashuuheinoMacBook-Pro.local>
2022-02-21 14:16:25 +0900

    add

--- src/identifyForM.sh
+++ src/identifyForM.sh
@@ -12,18 +12,18 @@ do
   do
     ./identify 2 $method
     if [ $numOfData -eq "1" ]; then
-        java evaluation/evaluation/EvaluationForM $numOfData $method $R $T $I> data/result/evaluation/move/$method.txt
+      java evaluation/evaluation/EvaluationForM $numOfData $method $R $T $I> data/result/evaluation/move/$method.txt
     else
-        java evaluation/evaluation/EvaluationForM $numOfData $method $R $T $I>> data/result/evaluation/move/$method.txt
+      java evaluation/evaluation/EvaluationForM $numOfData $method $R $T $I>> data/result/evaluation/move/$method.txt
     fi
   done
   for n in {1..100}
-    do
-        java identifyMacAddress/identify/IdentifyStay data/capture/convert/move/convert$n.csv $R $T > data/result/multi/move/$method/$n.txt
-    done
-    if [ $numOfData -eq "1" ]; then
-        java evaluation/evaluation/Evaluation100Old $R $T $numOfData > data/result/evaluation/move/$method.txt
-    else
-        java evaluation/evaluation/Evaluation100Old $R $T $numOfData >> data/result/evaluation/move/$method.txt
-    fi
+  do
+    java identifyMacAddress/identify/IdentifyStay data/capture/convert/move/convert$n.csv $R $T > data/result/multi/move/old/$n.txt
+  done
+  if [ $numOfData -eq "1" ]; then
+    java evaluation/evaluation/Evaluation100Old $R $T $numOfData > data/result/evaluation/move/old.txt
+  else
+    java evaluation/evaluation/Evaluation100Old $R $T $numOfData >> data/result/evaluation/move/old.txt
+  fi
 done
