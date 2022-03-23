67d0270ce45676f19940cec87124221cf1885c36
秋山周平 <akiyamashuuhei@akiyamashuuheinoMacBook-Pro.local>
2022-03-23 16:06:24 +0900

    add

--- src/compile.sh
+++ src/compile.sh
@@ -4,10 +4,10 @@
 find ./ -name "*.class" -delete
 javac dataAnalyze/DataAnalyze.java -encoding UTF-8
 javac evaluation/evaluation/*.java -encoding UTF-8
-javac /Users/akiyamashuuhei/data/IdentifyMacAddress/src/evaluation/table/move/*.java -encoding UTF-8
-javac /Users/akiyamashuuhei/data/IdentifyMacAddress/src/evaluation/table/stay/*.java -encoding UTF-8
-javac /Users/akiyamashuuhei/data/IdentifyMacAddress/src/graph/makeCDF/MakeCDF.java -encoding UTF-8
-javac /Users/akiyamashuuhei/data/IdentifyMacAddress/src/identifyMacAddress/identify/*.java -encoding UTF-8
+javac evaluation/table/move/*.java -encoding UTF-8
+javac evaluation/table/stay/*.java -encoding UTF-8
+javac graph/makeCDF/MakeCDF.java -encoding UTF-8
+javac identifyMacAddress/identify/*.java -encoding UTF-8
 javac processed/lineUp/*.java -encoding UTF-8
 javac processed/extract/*.java -encoding UTF-8
 javac processed/delay/*.java -encoding UTF-8
