29c831cf96fe48cc2024d8a87c410dcbe59e9f22
Atkiyama <79164316+Atkiyama@users.noreply.github.com>
2022-03-23 16:03:53 +0900

    add

--- src/compile.sh
+++ src/compile.sh
@@ -2,12 +2,13 @@
 
 #ディレクトリ内の全てのクラスファイルを削除後、再コンパイルします
 find ./ -name "*.class" -delete
-javac dataAnalyze/DataAnalyze.java -encoding UTF-8
-javac evaluation/evaluation/*.java -encoding UTF-8
-javac /Users/akiyamashuuhei/data/IdentifyMacAddress/src/evaluation/table/move/*.java -encoding UTF-8
-javac /Users/akiyamashuuhei/data/IdentifyMacAddress/src/evaluation/table/stay/*.java -encoding UTF-8
-javac /Users/akiyamashuuhei/data/IdentifyMacAddress/src/graph/makeCDF/MakeCDF.java -encoding UTF-8
-javac /Users/akiyamashuuhei/data/IdentifyMacAddress/src/identifyMacAddress/identify/*.java -encoding UTF-8
-javac processed/lineUp/*.java -encoding UTF-8
-javac processed/extract/*.java -encoding UTF-8
-javac processed/delay/*.java -encoding UTF-8
+javac dataAnalyze/DataAnalyze.java -encoding utf-8
+javac evaluation/evaluation/*.java -encoding utf-8
+javac evaluation/table/move/EvaluationTable.java -encoding utf-8
+javac evaluation/table/stay/EvaluationTable.java -encoding utf-8
+javac graph/makeCDF/MakeCDF.java -encoding utf-8
+javac identifyMacAddress/identify/IdentifyMove.java -encoding utf-8
+javac identifyMacAddress/identify/IdentifyStay.java -encoding utf-8
+javac processed/lineUp/*.java -encoding utf-8
+javac processed/extract/*.java -encoding utf-8
+javac processed/delay/*.java -encoding utf-8
