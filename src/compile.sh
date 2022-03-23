22d0c5b96cfaa6cb10d2d54957b46e457418d3f3
秋山周平 <akiyamashuuhei@akiyamashuuheinoMacBook-Pro.local>
2022-03-23 15:47:05 +0900

    add

--- src/compile.sh
+++ src/compile.sh
@@ -2,12 +2,12 @@
 
 #ディレクトリ内の全てのクラスファイルを削除後、再コンパイルします
 find ./ -name "*.class" -delete
-javac dataAnalyze/DataAnalyze.java
-javac evaluation/evaluation/*.java
-javac /Users/akiyamashuuhei/data/IdentifyMacAddress/src/evaluation/table/move/*.java
-javac /Users/akiyamashuuhei/data/IdentifyMacAddress/src/evaluation/table/stay/*.java
-javac /Users/akiyamashuuhei/data/IdentifyMacAddress/src/graph/makeCDF/MakeCDF.java
-javac /Users/akiyamashuuhei/data/IdentifyMacAddress/src/identifyMacAddress/identify/*.java
-javac processed/lineUp/*.java
-javac processed/extract/*.java
-javac processed/delay/*.java
+javac dataAnalyze/DataAnalyze.java -encoding UTF-8
+javac evaluation/evaluation/*.java -encoding UTF-8
+javac /Users/akiyamashuuhei/data/IdentifyMacAddress/src/evaluation/table/move/*.java -encoding UTF-8
+javac /Users/akiyamashuuhei/data/IdentifyMacAddress/src/evaluation/table/stay/*.java -encoding UTF-8
+javac /Users/akiyamashuuhei/data/IdentifyMacAddress/src/graph/makeCDF/MakeCDF.java -encoding UTF-8
+javac /Users/akiyamashuuhei/data/IdentifyMacAddress/src/identifyMacAddress/identify/*.java -encoding UTF-8
+javac processed/lineUp/*.java -encoding UTF-8
+javac processed/extract/*.java -encoding UTF-8
+javac processed/delay/*.java -encoding UTF-8
