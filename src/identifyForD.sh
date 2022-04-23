f6416fe08cc155bbd56a9ffd98cdf67cf2fc95ff
秋山周平 <akiyamashuuhei@akiyamashuuheinoMacBook-Pro.local>
2022-04-23 16:09:00 +0900

    add

--- src/identifyForD.sh
+++ src/identifyForD.sh
@@ -12,22 +12,19 @@ do
   for method in svr bagging linerRegression randomForest
   do
     ./identify 2 $method
-    if [ $numOfData -eq "1" ]; then
-      java evaluation/evaluation/EvaluationForM $numOfData $method $R $T $I> data/result/evaluation/move/D,$method.txt
+    if [ $delay -eq "0" ]; then
+      java evaluation/evaluation/EvaluationForM $delay $method $R $T $I> data/result/evaluation/move/D,$method.txt
     else
-      java evaluation/evaluation/EvaluationForM $numOfData $method $R $T $I>> data/result/evaluation/move/D,$method.txt
+      java evaluation/evaluation/EvaluationForM $delay $method $R $T $I>> data/result/evaluation/move/D,$method.txt
     fi
   done
   for n in {1..100}
   do
     java identifyMacAddress/identify/IdentifyStay data/capture/convert/move/$n,convertData.csv $R $T > data/result/multi/move/old/$n.txt
   done
-  if [ $numOfData -eq "1" ]; then
-    java evaluation/evaluation/EvaluationForM $numOfData old $R $T $I> data/result/evaluation/move/D,old.txt
-  elif [ "$numOfData" -eq "$79" ]
-    then
-    java evaluation/evaluation/EvaluationForMSingle $numOfData old $R $T $I>> data/result/evaluation/move/D,old.txt
+  if [ $delay -eq "0" ]; then
+    java evaluation/evaluation/EvaluationForM $delay old $R $T $I> data/result/evaluation/move/D,old.txt
   else
-    java evaluation/evaluation/EvaluationForM $numOfData old $R $T $I>> data/result/evaluation/move/D,old.txt
+    java evaluation/evaluation/EvaluationForM $delay old $R $T $I>> data/result/evaluation/move/D,old.txt
   fi
 done
