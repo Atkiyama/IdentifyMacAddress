80d3e8376986e6a0902395e31517a04a3a37e7f7
秋山周平 <akiyamashuuhei@akiyamashuuheinoMacBook-Pro.local>
2022-05-04 21:20:18 +0900

    add

--- src/identifyForC.sh
+++ src/identifyForC.sh
@@ -1,7 +1,7 @@
 #bin/bash
 
 #コマンドライン引数 1に回帰手法(旧手法の場合はold)
-for numOfData in {1..79}
+for numOfData in 79
 do
   java processed/lineUp/LineUp $numOfData
   R=15
@@ -17,17 +17,10 @@ do
   done
   for n in {1..100}
   do
-    if [ $numOfData -eq "79" ]; then
-      java identifyMacAddress/identify/IdentifyStay data/capture/convert/move/0,convertData.csv $R $T > data/result/multi/move/old/$n/$R,$T,$I.txt
-    else
-      java identifyMacAddress/identify/IdentifyStay data/capture/convert/move/$n,convertData.csv $R $T > data/result/multi/move/old/$n/$R,$T,$I.txt
-    fi
+    java identifyMacAddress/identify/IdentifyStay data/capture/convert/move/$n,convertData.csv $R $T > data/result/multi/move/old/$n/$R,$T,$I.txt
   done
   if [ $numOfData -eq "1" ]; then
     java evaluation/evaluation/EvaluationForM $numOfData old $R $T $I> data/result/evaluation/move/C,old.txt
-  elif [ "$numOfData" -eq "79" ]
-    then
-    java evaluation/evaluation/EvaluationForMSingle $numOfData old $R $T $I>> data/result/evaluation/move/C,old.txt
   else
     java evaluation/evaluation/EvaluationForM $numOfData old $R $T $I>> data/result/evaluation/move/C,old.txt
   fi
--- src/identifyForC_sub.sh
+++ src/identifyForC_sub.sh
@@ -7,16 +7,9 @@ R=$3
 T=$4
 I=$5
 python $method,2.py
-if [ $numOfData -eq "79" ]; then
-  ./identify 4 $method
-else
-  ./identify 2 $method
-fi
+./identify 2 $method
 if [ $numOfData -eq "1" ]; then
   java evaluation/evaluation/EvaluationForM $numOfData $method $R $T $I> data/result/evaluation/move/C,$method.txt
-elif [ "$numOfData" -eq "79" ]
-then
-  java evaluation/evaluation/EvaluationForMSingle $numOfData $method $R $T $I>> data/result/evaluation/move/C,$method.txt
 else
   java evaluation/evaluation/EvaluationForM $numOfData $method $R $T $I>> data/result/evaluation/move/C,$method.txt
 fi
